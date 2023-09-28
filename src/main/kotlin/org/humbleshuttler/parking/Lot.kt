67023bdcea2bc2976a45d9cf15849ec16b44a42a
Subrat Prasad <subratkprasad@gmail.com>
2023-09-28 10:40:51 -0400

    Minor improvements to get response format

--- src/main/kotlin/org/humbleshuttler/parking/Lot.kt
+++ src/main/kotlin/org/humbleshuttler/parking/Lot.kt
@@ -16,7 +16,7 @@ class Lot(val address: Address, private val parkingCount: Int) {
 
 
     override fun toString(): String {
-        return "LotId: $lotId\n" + "Address: $address" + getHistory()
+        return "LotId: $lotId\n" + "Address: $address\n" + "Available spots: ${emptySpaceCount.get()}" + getHistory()
     }
 
     fun isFull(): Boolean {
@@ -52,15 +52,13 @@ class Lot(val address: Address, private val parkingCount: Int) {
 
     private fun getHistory(): String {
         val builder = StringBuilder()
-        builder.append("===== History of parking lot =====")
-        builder.append("ReceiptId Receipt")
-        builder.append("\n")
+        builder.append("===== History of parking lot =====\n")
+        builder.append("ReceiptId Receipt\n")
         for (item in history) {
             builder.append(item.key + " " + item.value)
             builder.append("\n")
         }
-        builder.append("===== End of History =====")
-        builder.append("\n")
+        builder.append("===== End of History =====\n")
         return builder.toString()
     }
 
@@ -85,7 +83,9 @@ data class Address(
 ) {
     override fun toString(): String {
         val builder = StringBuilder(line1 + "\n")
-        builder.append(line2 + "\n")
+        if (line2.isNotEmpty()) {
+            builder.append(line2 + "\n")
+        }
         builder.append("city: $city\n")
         builder.append("state: $state\n")
         builder.append("country: $country\n")
